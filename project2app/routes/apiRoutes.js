6d4c91e1bd22d360507403233cae69522c9a59d2
B Lub <brandonlublin@D-10-18-224-114.dhcp4.washington.edu>
2019-06-08 11:18:17 -0700

    fixed database name

--- project2app/routes/apiRoutes.js
+++ project2app/routes/apiRoutes.js
@@ -17,8 +17,8 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExampleOne) {
+      res.json(dbExampleOne);
     });
   });
 };
