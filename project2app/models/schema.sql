fa71c8c19ab1c98348e49e2315504621ee8d90c0
B Lub <brandonlublin@D-10-19-128-182.dhcp4.washington.edu>
2019-06-11 18:36:35 -0700

    a few trial and error fixes for favicon, no real changes

--- project2app/models/schema.sql
+++ project2app/models/schema.sql
@@ -1,5 +1,14 @@
 DROP DATABASE IF EXISTS questions_db;
 CREATE DATABASE questions_db;
 
-DROP DATABASE IF EXISTS devquestions_db;
+-- DROP DATABASE IF EXISTS devquestions_db;
 CREATE DATABASE devquestions_db;
+
+use devquestions_db;
+-- Create the table in the specified schema
+CREATE TABLE questions (
+    id int NOT NULL AUTO_INCREMENT,
+    question varchar(300) NOT NULL,
+    answer varchar(30) NOT NULL,
+    PRIMARY KEY (id)
+);
