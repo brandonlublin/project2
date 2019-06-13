0d10ffdfc71219f4f46dd53103558de49c481d1f
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-06-12 20:04:07 -0700

    added answers and questions html routes/pages. no logic or formatting done

--- project2app/models/seeds.sql
+++ project2app/models/seeds.sql
@@ -1,2 +1,11 @@
 INSERT INTO questions (text, description, createdAt, updatedAt)
 VALUES ('Who was the third president of the United States?', 'Thomas Jefferson', '2019-06-10 19:59:59', '2019-06-10 19:59:59');
+
+INSERT INTO Answers (text, description, createdAt, updatedAt)
+VALUES ('James Madison', 'Brandon', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
+INSERT INTO answers (text, description, createdAt, updatedAt)
+VALUES ('Happy Gilmore', 'Gabbi', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
+INSERT INTO answers (text, description, createdAt, updatedAt)
+VALUES ('Fred Durst', 'Stephen', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
+INSERT INTO answers (text, description, createdAt, updatedAt)
+VALUES ('Nickelback', 'Kurtis', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
