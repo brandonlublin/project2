934d8dbb9baeef65d00b2c5c1b4cab87033cb19a
gabbistein <gabbistein@outlook.com>
2019-06-13 21:19:29 -0700

    integration of socket.io, update of .gitignore, updating of handlebars

--- project2app/models/seeds.sql
+++ project2app/models/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO questions (text, description, createdAt, updatedAt)
 VALUES ('Who was the third president of the United States?', 'Thomas Jefferson', '2019-06-10 19:59:59', '2019-06-10 19:59:59');
 
-INSERT INTO Answers (text, description, createdAt, updatedAt)
+INSERT INTO answers (text, description, createdAt, updatedAt)
 VALUES ('James Madison', 'Brandon', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
 INSERT INTO answers (text, description, createdAt, updatedAt)
 VALUES ('Happy Gilmore', 'Gabbi', '2019-06-10 19:59:59', '2019-06-10 19:59:59')
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 var db = require("./project2app/models");
+var socket = require('socket.io');
+var server;
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,6 +22,25 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+  console.log('made socket connection', socket.id);
+
+  // Handle chat event
+  socket.on('chat', function (data) {
+    // console.log(data);
+    io.sockets.emit('chat', data);
+  });
+
+  // Handle typing event
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
+  });
+
+});
+
 // Routes
 require("./project2app/routes/apiRoutes")(app);
 require("./project2app/routes/htmlRoutes")(app);
@@ -33,8 +54,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  server = app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
