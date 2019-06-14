092baeeb75712fe28a0f898be70d9be9359c3c37
B Lub <brandonlublin@D-10-19-28-171.dhcp4.washington.edu>
2019-06-13 20:14:10 -0700

    added my different routes for user data, questions, answers

--- project2app/routes/htmlRoutes.js
+++ project2app/routes/htmlRoutes.js
@@ -3,22 +3,31 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+  app.get("/question", function(req, res) {
     db.Question.findOne({}).then(function(dbQuestions) {
       // res.json(dbQuestions);
-      res.render("index", {
-        text: dbQuestions.text,
-        description: dbQuestions.description
+      res.render("Question", {
+        text: dbQuestions.text
       });
+    });
+
+    db.Answer.create({
+      text: "Test",
+      description: "Brandon",
+      include: ["Questions"]
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbAnswer) {
-    //   res.render("", {
-    //     example: dbAnswer
-    //   });
-    // });
+  app.get("/answers", function(req, res) {
+    db.Answer.findOne({}).then(function(dbAnswer) {
+      res.render("Answers", {
+        example: dbAnswer
+      });
+    });
   });
 
   // Render 404 page for any unmatched routes
