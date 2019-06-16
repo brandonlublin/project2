4478a61845993d75cb1f205c55ba92ab8d7db2e5
SITRUK <kurtis.santillanes@gmail.com>
2019-06-16 12:54:53 -0700

    adding slight changes to socket.io, updating cdn and order. also found typo in sql users.js file

--- project2app/models/user.js
+++ project2app/models/user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
 
   User.associate = function(models) {
     User.hasMany(models.UserAnswer, {
-      foreignKey: "UserAnserId",
+      foreignKey: "UserAnswerId",
       onDelete: "cascade"
     });
   };
