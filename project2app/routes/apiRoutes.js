eb1e5d57daa00f1f3443c59fbed52a866a8b7fab
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-06-17 21:40:54 -0700

    user answer is being pulled from input and stored in db. each user answer is aligning with user id and game id properly. timeout for user entry is working and all loading screens working as well

--- project2app/routes/apiRoutes.js
+++ project2app/routes/apiRoutes.js
@@ -1,128 +1,181 @@
-var db = require("../models");
-const Sequelize = require("sequelize");
+var db = require('../models');
+const Sequelize = require('sequelize');
 const op = Sequelize.Op;
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/trivia/:id", function(req, res) {
+  app.get('/api/trivia/:id', function(req, res) {
     db.Questions.findOne({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     }).then(function(getQuestionDb) {
       res.json(getQuestionDb);
     });
   });
   // Create a new example
-  app.post("/api/answers", function(req, res) {
-    db.Questions.create(req.body).then(function(userAnsDb) {
-      res.json(userAnsDb);
+
+  app.post('/api/useranswer/', function(req, res) {
+    console.log(req.body);
+    db.UserAnswer.create({
+      userAnswer: req.body.userAnswer,
+      TriviumId: parseInt(req.body.questionId),
+      UserId: parseInt(req.body.userId),
+      GameId: parseInt(req.body.gameId),
+      username: req.body.username,
+    }).then(function(response) {
+      // console.log(response.dataValues);
+      db.UserAnswer.findAll({
+        where: {
+          GameId: parseInt(req.body.gameId),
+          TriviumId: parseInt(req.body.questionId),
+        },
+      }).then(function(triviaRes) {
+        if (triviaRes.length < 4) {
+          res.json(false);
+        } else {
+          res.json(true);
+        }
+      });
     });
   });
 
-function test1(trivia, cd ){
-  let triviaIds = []; 
+  app.get('/api/useranswer/:gameid/:questionid', function(req, res) {
+    db.UserAnswer.findAll({
+      where: {
+        GameId: parseInt(req.params.gameid),
+        TriviumId: parseInt(req.params.questionid),
+      },
+    }).then(function(triviaRes) {
+      if (triviaRes.length < 4) {
+        res.json(false);
+      } else {
+        res.json(true);
+      }
+    });
+  });
+
+  app.post('/api/user', function(req, res) {
+    db.Game.findOne({
+      where: {
+        userCount: { [op.lt]: 4 },
+      },
+      order: [['createdAt', 'ASC']],
+    }).then(function(game) {
+      if (game === null) {
+        //query trivia
+        //query trivia table
+        db.Trivia.findAll().then(function(trivia) {
+          //match the game id with the questions for that game in the trivia table
+          matchGameIds(trivia, function(triviaIds) {
+            //run validate game capacity function(ensures there are 4 players in the game)
+            validateGameCapacity(triviaIds, function(triviaGameIds) {
+              //create game for user
+              db.Game.create({
+                triviaIds: triviaGameIds,
+              }).then(function(game) {
+                //create user for game
+                createNewUser(req, game, res);
+              });
+            });
+          });
+        });
+      } else {
+        //if no game found, just create new user
+        createNewUser(req, game, res);
+      }
+    });
+  });
+
+  // check user count to move users into game
+  app.get('/api/game/:id/status', function(req, res) {
+    db.Game.findByPk(req.params.id).then(function(game) {
+      var gameUserCount = game.dataValues.userCount;
+      if (gameUserCount > 3) {
+        res.json(true);
+      } else {
+        res.json(false);
+      }
+    });
+  });
+};
+
+function matchGameIds(trivia, cd) {
+  let triviaIds = [];
 
   trivia.forEach(function(question) {
     triviaIds.push(question.dataValues.id);
-  })
+  });
 
-  cd(triviaIds)
+  cd(triviaIds);
 }
 
-function test(triviaIds, cb){
+function validateGameCapacity(triviaIds, cb) {
   let triviaGameIds = [];
 
   while (triviaGameIds.length < 4) {
-
-    let index = Math.floor(Math.random() * (triviaIds.length - 1))
+    let index = Math.floor(Math.random() * (triviaIds.length - 1));
     let id = triviaIds[index];
 
-    if(!triviaGameIds.includes(id)){
+    if (!triviaGameIds.includes(id)) {
       triviaGameIds.push(id);
-    } 
+    }
   }
-  
-  triviaGameIds = triviaGameIds.toString()
-  cb(triviaGameIds)
+
+  triviaGameIds = triviaGameIds.toString();
+  cb(triviaGameIds);
 }
 
-  app.post("/api/user", function(req, res) {
-    db.Game.findOne({
-      where: {
-        userCount: { [op.lt] : 4 }
-      },
-      order: [[
-        "createdAt", "ASC"
-      ]]
-    }).then(function(game) {
-      if (game === null) {
-        //query trivia 
-        db.Trivia.findAll().then(function(trivia) {
-        test1(trivia, function(triviaIds){
-        
-          test(triviaIds, function(triviaGameIds){
-            db.Game.create({
-              triviaIds: triviaGameIds
-            }).then(function(game) {
-              createNewUser(req, game, res)
-
-            })
-          
-          })
-        })
-        })
-      
-      } 
-      else {
-        createNewUser(req, game, res)
-      }
-      
-    })
-    
-  })
-};
-
-function createNewUser(req, game, res){
+function createNewUser(req, game, res) {
   console.log(req);
-  
+  //update User table with username and sync with game ID they are joining
   db.User.create({
     username: req.body.username,
-    GameId: game.dataValues.id
+    GameId: game.dataValues.id,
   }).then(function(user) {
     updateGameUser(user.dataValues, game.dataValues, res);
-  })
+  });
 }
 
-function updateGameUser(user, game, res){
+function updateGameUser(user, game, res) {
   var newUserIds;
-  if(game.userIds) {
+  if (game.userIds) {
     var prevUserId = game.userIds;
-    newUserIds = prevUserId +","+ user.id;
+    newUserIds = prevUserId + ',' + user.id;
   } else {
-    newUserIds =  user.id.toString();
+    newUserIds = user.id.toString();
   }
   var newCount = parseInt(game.userCount) + 1;
 
   db.Game.update(
     {
       userIds: newUserIds,
-      userCount: newCount
+      userCount: newCount,
+      round: 1,
     },
     {
-    where: {
-      id: game.id
+      where: {
+        id: game.id,
+      },
     }
-  })
-  .then(function(updatedGame){
-    console.log(updatedGame)
-    // if newCount is 4 or more than start game;
-    if(newCount > 3) {
-      
-      res.json({gameid: game.id, userid:user.id, status: true});
+  ).then(function(updatedGame) {
+    console.log(updatedGame);
+    // if newCount is 4 or more than start new game;
+    if (newCount > 3) {
+      res.json({
+        gameid: game.id,
+        userid: user.id,
+        status: true,
+        username: user.username,
+      });
     } else {
-      res.json({gameid: game.id, userid:user.id, status: false}); 
+      res.json({
+        gameid: game.id,
+        userid: user.id,
+        status: false,
+        username: user.username,
+      });
     }
     // else show loading message to user waiting for more users to join
-  })
+  });
 }
--- project2app/routes/htmlRoutes.js
+++ project2app/routes/htmlRoutes.js
@@ -1,120 +1,119 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    res.render("index");
+  app.get('/', function(req, res) {
+    res.render('index');
   });
 
-  app.get("/roundResults", function(req, res) {
-      res.render("RoundResults")
+  app.get('/roundResults', function(req, res) {
+    res.render('RoundResults');
   });
 
-  app.get("/gameResults", function(req, res) {
-    res.render("GameResults")
+  app.get('/gameResults', function(req, res) {
+    res.render('GameResults');
   });
 
-  app.get("/answerChoice", function(req, res) {
-    res.render("AnswerChoice")
-  });
-  
-  app.get("/question", function(req, res) {
-    db.Trivia.findAll({}).then(function(dbQuestions) {
-      let questionObj = {
-        questions: []
-      }
-      var randomIndex = Math.floor(Math.random()*Object.keys(questionObj.questions).length)
-      let parsedQuestions = JSON.stringify(questionObj.questions)
-      dbQuestions.forEach(function(question) {
-        questionObj.questions.push(question.dataValues)
-        
-      })
-
-      questionObj.questions.splice(randomIndex)
-      console.log(parsedQuestions);
-      
-      // console.log("Question obj: " + JSON.stringify(questionObj));
-      res.render("Question", {
-        questions: JSON.stringify(questionObj.questions)
+  app.get('/play/:gameid/:questionid', function(req, res) {
+    db.UserAnswer.findAll({
+      where: {
+        GameId: parseInt(req.params.gameid),
+        TriviumId: parseInt(req.params.questionid),
+      },
+    }).then(function(triviaRes) {
+      var answers = [];
+      triviaRes.forEach(function(answer) {
+        answers.push(answer.dataValues);
+        if (answers.length === 4) {
+          res.render('AnswerChoice', { userAnswer: answer });
+        }
       });
     });
-    
-    
   });
 
+  app.get('/question', function(req, res) {
+    db.Trivia.findAll({}).then(function(dbQuestions) {
+      let questionObj = {
+        questions: [],
+      };
+      var randomIndex = Math.floor(
+        Math.random() * Object.keys(questionObj.questions).length
+      );
+      let parsedQuestions = JSON.stringify(questionObj.questions);
+      dbQuestions.forEach(function(question) {
+        questionObj.questions.push(question.dataValues);
+      });
 
-  app.get("/play/:gameid", function(req, res){
+      questionObj.questions.splice(randomIndex);
+      console.log(parsedQuestions);
+
+      // console.log("Question obj: " + JSON.stringify(questionObj));
+      res.render('Question', {
+        questions: JSON.stringify(questionObj.questions),
+      });
+    });
+  });
+
+  app.get('/play/:gameid', function(req, res) {
     /*
       find game by id
       get triviaIds split and for each find trivia by id create an obj with user id, round, and all the trivia data
     */
-    db.Game.findByPk(req.params.gameid)
-      .then(function(game){
-        var gameData = {
-          trivia: [],
-          userIds: game.dataValues.userIds.split(","),
-          triviaIds: game.dataValues.triviaIds.split(","),
-          round: game.dataValues.round,
-          id: game.dataValues.id,
-          userCount: game.dataValues.userCount
-        }
-      //  console.log(gameData.triviaIds)
-      //  console.log(gameData.triviaIds.length)
-      for(var i = 0; i < 4; i++){
-        var id = parseInt( gameData.triviaIds[i]);
-    
-        getTrivia(id)
-          .then(function(triviaData){
-            // console.log("triviaData", triviaData)
-            gameData.trivia.push(triviaData)
-            console.log(gameData.trivia)
-            if(gameData.trivia.length === 4){
-              res.render('game', gameData);
-            }
-          })
-      }
-    })
-  })
+    db.Game.findByPk(req.params.gameid).then(function(game) {
+      var gameData = {
+        round: game.dataValues.round,
+        questionObj: null,
+        userIds: game.dataValues.userIds.split(','),
+        triviaIds: game.dataValues.triviaIds.split(','),
+        id: game.dataValues.id,
+        userCount: game.dataValues.userCount,
+      };
+      var id = gameData.triviaIds[gameData.round - 1];
+      console.log(gameData.round - 1);
+      getTrivia(id).then(function(triviaData) {
+        gameData.questionObj = triviaData;
 
-function cycleTriviaIds(gameData, cb){
-  for(var i = 0; i < gameData.triviaIds.length; i++){
-    var id = parseInt( gameData.triviaIds[i]);
-    getTrivia(id)
-      .then(function(triviaData){
-        gameData.trivia.push(triviaData)
-      })
-  }
-  cb()
-}
-
-function getTrivia(id){
-  var id = parseInt(id)
- 
-  return db.Trivia.findByPk(id)
-  .then(function(trivia){
-    return trivia.dataValues
-  })
-}
-
-
-  app.get("/answerChoice", function(req, res) {
-    db.UserAnswer.findAll({}).then(function(dbAnswer) {
-      res.render("AnswerChoice", {
-        userAnswer: dbAnswer.userAnswer
+        res.render('game', gameData);
       });
     });
   });
 
-  app.get("/roundResults", function(req, res) {
+  function cycleTriviaIds(gameData, cb) {
+    for (var i = 0; i < gameData.triviaIds.length; i++) {
+      var id = parseInt(gameData.triviaIds[i]);
+      getTrivia(id).then(function(triviaData) {
+        gameData.trivia.push(triviaData);
+      });
+    }
+    cb();
+  }
+
+  function getTrivia(id) {
+    var id = parseInt(id);
+
+    return db.Trivia.findByPk(id).then(function(trivia) {
+      return trivia.dataValues;
+    });
+  }
+
+  app.get('/answerChoice', function(req, res) {
     db.UserAnswer.findAll({}).then(function(dbAnswer) {
-      res.render("RoundResults", {
-        userAnswer: dbAnswer.userAnswer
+      res.render('AnswerChoice', {
+        userAnswer: dbAnswer.userAnswer,
+      });
+    });
+  });
+
+  app.get('/roundResults', function(req, res) {
+    db.UserAnswer.findAll({}).then(function(dbAnswer) {
+      res.render('RoundResults', {
+        userAnswer: dbAnswer.userAnswer,
       });
     });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get('*', function(req, res) {
+    res.render('404');
   });
 };
